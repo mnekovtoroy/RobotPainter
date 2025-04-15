7d59a34213e8e469947325813e07b7d04adf2081
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-04-15 15:04:35 +0300

    Visualization refactor + voronoi vizualizer

---
+++ RobotPainter.ConsoleTest/TestCases.cs
@@ -0,0 +1,80 @@
+﻿using System.Drawing;
+using RobotPainter.Calculations;
+using RobotPainter.Visualization;
+
+namespace RobotPainter.ConsoleTest
+{
+    public static class TestCases
+    {
+        public static void ColorConversionTest()
+        {
+            Color test = Color.FromArgb(56, 23, 117);
+            ColorLab colorLab = ColorLab.FromRgb(test);
+            Color reverse = colorLab.ToRgb();
+
+            Console.WriteLine($"Original:\n\tL: {test.R}\ta: {test.G}\tb: {test.B}");
+            Console.WriteLine($"L*a*b*:\n\tL: {colorLab.L}\ta: {colorLab.a}\tb: {colorLab.b}");
+            Console.WriteLine($"Reversed:\n\tL: {reverse.R}\ta: {reverse.G}\tb: {reverse.B}");
+        }
+
+        public static void PaletteApplianceTest()
+        {
+            string path = @"C:\Users\User\source\repos\RobotPainter\RobotPainter.ConsoleTest\test_images\";
+            Bitmap original = new Bitmap(path + "test5.jpg");
+            original.Save(path + "1_original.png");
+
+            LabBitmap lbm = new LabBitmap(original);
+            Bitmap reverse = lbm.ToBitmap();
+            reverse.Save(path + "2_reverse.png");
+
+            Palette plt = new Palette();
+            plt.Colors.Add(ColorLab.FromRgb(Color.White));
+            plt.Colors.Add(ColorLab.FromRgb(Color.LightGray));
+            plt.Colors.Add(ColorLab.FromRgb(Color.Gray));
+            plt.Colors.Add(ColorLab.FromRgb(Color.DarkGray));
+            plt.Colors.Add(ColorLab.FromRgb(Color.Black));
+
+            Bitmap plted = plt.Apply(lbm).ToBitmap();
+            plted.Save(path + "3_paletted.png");
+        }
+
+        public static void VectorTest()
+        {
+            string path = @"C:\Users\User\source\repos\RobotPainter\RobotPainter.ConsoleTest\test_images\";
+            Bitmap image = new Bitmap(path + "test_ball.jpg");
+
+            int n_avg = 3;
+            int n_arrows = 20;
+
+            int l_s = image.Width < image.Height ? image.Width : image.Height;
+            int arrow_length = (l_s / 20) / 3;
+            int tip_length = arrow_length / 4;
+
+            LabBitmap lbmp = new LabBitmap(image);
+
+            double[,] u, v;
+            (u, v) = ImageProcessor.LNormWithRollAvg(lbmp, n_avg);
+            //visualise
+            VectorVisualizer.VisualizeVectorsInline(image, u, v, n_arrows, Color.Blue, Color.Blue, arrow_length, tip_length);
+
+            image.Save(path + "norm_visualised.png");
+        }
+
+        public static void VoroniVisualizerTest()
+        {
+            string path = @"C:\Users\User\source\repos\RobotPainter\RobotPainter.ConsoleTest\test_images\";
+            Bitmap image = new Bitmap(path + "test_ball.jpg");
+
+            var sites = Voronoi.GenerateRandomMesh(200, image.Width, image.Height);
+
+            Bitmap voronoi = new Bitmap(image.Width, image.Height);
+            voronoi.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            int centroid_r = 1;
+            Color edge_c = Color.Blue;
+            Color centroid_c = Color.Red;
+            VoronoiVisualizer.VisualizeVoronoiInline(image, sites, edge_c, centroid_c, centroid_r);
+            image.Save(path + "test_voronoi.png");
+        }
+    }
+}
---
+++ RobotPainter.Visualization/VoronoiVisualizer.cs
@@ -0,0 +1,34 @@
+﻿using SharpVoronoiLib;
+using System.Drawing;
+
+namespace RobotPainter.Visualization
+{
+    public static class VoronoiVisualizer
+    {
+        public static void VisualizeVoronoiInline(Bitmap image, List<VoronoiSite> sites, Color edge_c, Color centroid_c, int centroid_r = 1)
+        {
+            using(var g = Graphics.FromImage(image))
+            {
+                Pen p = new Pen(edge_c);
+                Brush b = new SolidBrush(centroid_c);
+                foreach (var site in sites)
+                {
+                    var edges = site.Cell;
+                    foreach (var edge in edges)
+                    {
+                        int x0, y0, x1, y1;
+                        x0 = Convert.ToInt32(edge.Start.X);
+                        x1 = Convert.ToInt32(edge.Start.Y);
+                        y0 = Convert.ToInt32(edge.End.X);
+                        y1 = Convert.ToInt32(edge.End.Y);
+                        g.DrawLine(p, x0, x1, y0, y1);
+                    }
+                    int px, py;
+                    px = Convert.ToInt32(site.Centroid.X);
+                    py = Convert.ToInt32(site.Centroid.Y);
+                    g.FillEllipse(b, px - centroid_r, py - centroid_r, centroid_r * 2, centroid_r * 2);
+                }
+            }
+        }
+    }
+}

a49102b93e084ef5d17702a6de11bc197c95a50a
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-04-18 21:01:29 +0300

    +PointD
    Im tired of (double, double)

---
+++ RobotPainter.Calculations/PointD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotPainter.Calculations
+{
+    public struct PointD : IEquatable<PointD>
+    {
+        double x, y;
+
+        public PointD(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static PointD Add(PointD a, PointD b) => new PointD(a.x + b.x, a.y + b.y);
+
+        public static PointD Substract(PointD a, PointD b) => new PointD(a.x - b.x, a.y - b.y);
+
+        public static PointD operator +(PointD a, PointD b) => Add(a, b);
+
+        public static PointD operator -(PointD a, PointD b) => Substract(a, b);
+
+        public static bool operator ==(PointD a, PointD b) => a.x == b.x && a.y == b.y;
+
+        public static bool operator !=(PointD a, PointD b) => a.x != b.x || a.y != b.y;
+
+        public override readonly bool Equals(object? obj) => obj is PointF && Equals((PointF)obj);
+
+        public readonly bool Equals(PointD other) => this == other;
+
+        public override readonly int GetHashCode() => HashCode.Combine(x.GetHashCode(), y.GetHashCode());
+    }
+}

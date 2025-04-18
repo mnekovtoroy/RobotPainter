ba6573ffc473cc70ea8c39cb64108bbe5e6af59c
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-04-18 15:12:37 +0300

    introducing new Brushstroke generator

---
+++ RobotPainter.Calculations/StrokeGeneration/Brushstroke.cs
@@ -0,0 +1,174 @@
+﻿using SharpVoronoiLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotPainter.Calculations.StrokeGeneration
+{
+    public class Brushstroke
+    {
+        public readonly StrokeGenerator _strokeGenerator;
+
+        public List<VoronoiSite> involvedSites;
+
+        public VoronoiSite startingPoint;
+
+
+        public Brushstroke(StrokeGenerator parent, VoronoiSite starting_point)
+        {
+            _strokeGenerator = parent;
+            startingPoint = starting_point;
+        }
+
+        public void GenerateStroke(double max_length, double max_width, double L_tol)
+        {
+            involvedSites = new List<VoronoiSite>();
+            involvedSites.Add(startingPoint);
+
+            double curr_length = 0.0;
+
+            //forward expansion
+            VoronoiSite last_site = startingPoint;
+            var last_centroid = last_site.Centroid;
+            while (curr_length < max_length)
+            {
+                int lc_ix = Convert.ToInt32(last_centroid.X);
+                int lc_iy = Convert.ToInt32(last_centroid.Y);
+
+                double vx = _strokeGenerator.u[lc_ix, lc_iy];
+                double vy = _strokeGenerator.v[lc_ix, lc_iy];
+
+                VoronoiSite next_site = GetAdjasentSite(last_site, vx, vy);
+
+                if (!IsValidSite(next_site)) 
+                    break;
+                var next_centroid = next_site.Centroid;
+                if (!IsValidExpand(last_centroid, next_centroid, curr_length, max_length, L_tol))
+                    break;
+
+                curr_length += Math.Sqrt(Math.Pow(next_centroid.X - last_centroid.X, 2) + Math.Pow(next_centroid.Y - last_centroid.Y, 2));
+                involvedSites.Add(next_site);
+                last_site = next_site;
+                last_centroid = next_centroid;
+            }
+
+            //backwards expansion
+            last_site = startingPoint;
+            last_centroid = last_site.Centroid;
+            while (curr_length < max_length)
+            {
+                int lc_ix = Convert.ToInt32(last_centroid.X);
+                int lc_iy = Convert.ToInt32(last_centroid.Y);
+
+                double vx = -_strokeGenerator.u[lc_ix, lc_iy];
+                double vy = -_strokeGenerator.v[lc_ix, lc_iy];
+
+                VoronoiSite next_site = GetAdjasentSite(last_site, vx, vy);
+
+                if (!IsValidSite(next_site))
+                    break;
+                var next_centroid = next_site.Centroid;
+                if (!IsValidExpand(last_centroid, next_centroid, curr_length, max_length, L_tol))
+                    break;
+
+                curr_length += Math.Sqrt(Math.Pow(next_centroid.X - last_centroid.X, 2) + Math.Pow(next_centroid.Y - last_centroid.Y, 2));
+                involvedSites.Insert(0, next_site);
+                last_site = next_site;
+                last_centroid = next_centroid;
+            }
+        }
+
+        private bool IsValidExpand(VoronoiPoint last_centroid, VoronoiPoint next_centroid, double curr_length, double max_length, double L_tol)
+        {
+            int lc_ix = Convert.ToInt32(last_centroid.X);
+            int lc_iy = Convert.ToInt32(last_centroid.Y);
+
+            int nc_ix = Convert.ToInt32(next_centroid.X);
+            int nc_iy = Convert.ToInt32(next_centroid.Y);
+
+            double last_c_L = _strokeGenerator.image.GetPixel(lc_ix, lc_iy).L;
+            double next_c_L = _strokeGenerator.image.GetPixel(nc_ix, nc_iy).L;
+
+            if (Math.Abs(next_c_L - last_c_L) > L_tol)
+            {
+                return false;
+            }
+
+            double add_length = Math.Sqrt(Math.Pow(next_centroid.X - last_centroid.X, 2) + Math.Pow(next_centroid.Y - last_centroid.Y, 2));
+            if (curr_length + add_length < max_length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidSite(VoronoiSite site)
+        {
+            return site != null && !involvedSites.Contains(site) && !_strokeGenerator.IsSiteReserved(site);
+        }
+
+        private VoronoiSite GetAdjasentSite(VoronoiSite site, double dx, double dy)
+        {
+            var centroid = site.Centroid;
+            var edges = site.Cell;
+            foreach (var edge in edges)
+            {
+                if (CheckIntersection(
+                    centroid.X, centroid.Y, dx, dy,
+                    edge.Start.X, edge.Start.Y, edge.End.X, edge.End.Y))
+                {
+                    //check first to the left of the edge, then to the right of the edge
+                    if (edge.Left != null && edge.Left != site)
+                    {
+                        return edge.Left;
+                    }
+                    else if (edge.Right != null && edge.Right != site)
+                    {
+                        return edge.Right;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static bool CheckIntersection(
+        double v_x0, double v_y0, double v_dx, double v_dy,
+        double s_x0, double s_y0, double s_x1, double s_y1)
+        {
+            double s_dx = s_x1 - s_x0;
+            double s_dy = s_y1 - s_y0;
+
+            // check if vector and segment are parallel
+            double cross = v_dx * s_dy - v_dy * s_dx;
+            if (Math.Abs(cross) < 1e-10)
+            {
+                // check if they are on the same line
+                double dx = s_x0 - v_x0;
+                double dy = s_y0 - v_y0;
+                double cross2 = dx * v_dy - dy * v_dx;
+                if (Math.Abs(cross2) < 1e-10)
+                {
+                    // check if there is overlap between them
+                    double t0 = (s_x0 - v_x0) * v_dx + (s_y0 - v_y0) * v_dy;
+                    double t1 = (s_x1 - v_x0) * v_dx + (s_y1 - v_y0) * v_dy;
+                    return t0 >= 0 || t1 >= 0;
+                }
+                return false;
+            }
+
+            
+            double s_T = ((v_x0 - s_x0) * v_dy - (v_y0 - s_y0) * v_dx) / cross;
+            double v_T = ((v_x0 - s_x0) * s_dy - (v_y0 - s_y0) * s_dx) / cross;
+
+            //check that intersection is on the ray (from the vector) and on the 
+            return s_T >= 0 && s_T <= 1 && v_T >= 0;
+        }
+    }
+}
---
+++ RobotPainter.Calculations/StrokeGeneration/StrokeGenerator.cs
@@ -0,0 +1,107 @@
+﻿using RobotPainter.Calculations.Optimization;
+using SharpVoronoiLib;
+
+namespace RobotPainter.Calculations.StrokeGeneration
+{
+    public class StrokeGenerator
+    {
+        public static List<VoronoiSite> GenerateRandomRelaxedMesh(int n, int width, int height, int relax_iterations = 3)
+        {
+            VoronoiPlane plane = new VoronoiPlane(0, 0, width - 1, height - 1);
+            plane.GenerateRandomSites(n, PointGenerationMethod.Uniform);
+            plane.Tessellate();
+            plane.Relax(relax_iterations);
+            return plane.Sites;
+        }
+
+        public List<VoronoiSite> sites;
+
+        public Dictionary<VoronoiSite, int> strokeMask; 
+
+        public LabBitmap image;
+        public double[,] u;
+        public double[,] v;
+
+        private int width;
+        private int height;
+
+        private readonly IOptimizer _optimizer;
+
+        private List<Brushstroke> strokes = new List<Brushstroke>();
+
+        public StrokeGenerator(LabBitmap target_image, int n_voronoi, IOptimizer optimizer, int n_rolling_avg = 7)
+        {
+            image = target_image;
+            (u,v) = ImageProcessor.LNormWithRollAvg(image, n_rolling_avg);
+            width = image.Width;
+            height = image.Height;
+            _optimizer = optimizer;
+
+            sites = GenerateRandomRelaxedMesh(n_voronoi, width, height);
+        }
+
+        public List<Brushstroke> CalculateStorkes()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<(double, double)> CalculateSitesLPull()
+        {
+            List<(double, double)> Lpull = new List<(double, double)>();
+            foreach(var site in sites)
+            {
+                double x_pull = 0.0, y_pull = 0.0;
+                //possible optimization: use site.Cell instead of points
+                var points = site.Points;
+                var centroid = site.Centroid;
+                int cx = Convert.ToInt32(centroid.X);
+                int cy = Convert.ToInt32(centroid.Y);
+                foreach(var point in site.Points)
+                {
+                    int px = Convert.ToInt32(point.X);
+                    int py = Convert.ToInt32(point.Y);
+                    double L_diff = Math.Abs(image.GetPixel(px, py).L - image.GetPixel(cx, cy).L);
+                    x_pull += -(point.X - centroid.X) * L_diff / points.Count();
+                    y_pull += -(point.Y - centroid.Y) * L_diff / points.Count();
+                }
+                Lpull.Add((x_pull, y_pull));
+            }
+            return Lpull;
+        }
+
+        private void PullSites(List<(double, double)> sites_pull, double strength = 1.0)
+        {
+            var new_sites = new List<VoronoiSite>();
+            for (int i = 0; i < sites.Count; i++)
+            {
+                new_sites.Add(new VoronoiSite(sites[i].X + strength * sites_pull[i].Item1, sites[i].Y + strength * sites_pull[i].Item2));
+            }
+            sites = new_sites;
+            VoronoiPlane.TessellateOnce(sites, 0, 0, width - 1, height - 1);
+        }
+
+        public void PerformLPull(double str_Lpull = 0.2, double str_centroid = 0.5)
+        {
+            var Lpull = CalculateSitesLPull();
+            for (int i = 0; i < sites.Count; i++)
+            {
+                //pull to centroid + away from color diff
+                var centroid = sites[i].Centroid;
+                double pull_x = str_Lpull * Lpull[i].Item1 + str_centroid * (centroid.X - sites[i].X);
+                double pull_y = str_Lpull * Lpull[i].Item2 + str_centroid * (centroid.X - sites[i].X);
+                Lpull[i] = (pull_x, pull_y);
+            }
+            PullSites(Lpull);
+        }
+
+        public Brushstroke GenerateBrushstroke()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSiteReserved(VoronoiSite site)
+        {
+            return strokeMask.ContainsKey(site);
+        }
+    }
+}

19aa744fff45d40706ba239020e762f427fa0c91
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-04-18 19:20:07 +0300

    +new mapping technique and checking for intersection technique

--- RobotPainter.Calculations/StrokeGeneration/Brushstroke.cs
+++ RobotPainter.Calculations/StrokeGeneration/Brushstroke.cs
@@ -98,7 +98,7 @@ namespace RobotPainter.Calculations.StrokeGeneration
             }
 
             double add_length = Math.Sqrt(Math.Pow(next_centroid.X - last_centroid.X, 2) + Math.Pow(next_centroid.Y - last_centroid.Y, 2));
-            if (curr_length + add_length < max_length)
+            if (curr_length + add_length > max_length)
             {
                 return false;
             }
@@ -140,36 +140,45 @@ namespace RobotPainter.Calculations.StrokeGeneration
         }
 
         private static bool CheckIntersection(
-        double v_x0, double v_y0, double v_dx, double v_dy,
-        double s_x0, double s_y0, double s_x1, double s_y1)
+            double v_x0, double v_y0, double v_dx, double v_dy,
+            double s_x0, double s_y0, double s_x1, double s_y1)
         {
+            //formulas: http://e-maxx.ru/algo/lines_intersection
             double s_dx = s_x1 - s_x0;
             double s_dy = s_y1 - s_y0;
 
-            // check if vector and segment are parallel
-            double cross = v_dx * s_dy - v_dy * s_dx;
-            if (Math.Abs(cross) < 1e-10)
+            double a_v, b_v, c_v;
+            double a_s, b_s, c_s;
+
+            (a_v, b_v, c_v) = GetLine(v_x0, v_y0, v_x0 + v_dx, v_y0 + v_dy);
+            (a_s, b_s, c_s) = GetLine(s_x0, s_y0, s_x1, s_y1);
+
+            double den = a_v * b_s - a_s * b_v;
+            if (den == 0)
             {
-                // check if they are on the same line
-                double dx = s_x0 - v_x0;
-                double dy = s_y0 - v_y0;
-                double cross2 = dx * v_dy - dy * v_dx;
-                if (Math.Abs(cross2) < 1e-10)
-                {
-                    // check if there is overlap between them
-                    double t0 = (s_x0 - v_x0) * v_dx + (s_y0 - v_y0) * v_dy;
-                    double t1 = (s_x1 - v_x0) * v_dx + (s_y1 - v_y0) * v_dy;
-                    return t0 >= 0 || t1 >= 0;
-                }
+                //can check if they're on the same line, but i dont think its important
                 return false;
             }
+            //fidning intersection point
+            double x_i = -(c_v * b_s - c_s * b_v) / den;
+            double y_i = -(a_v * c_s - a_s * c_v) / den;
 
-            
-            double s_T = ((v_x0 - s_x0) * v_dy - (v_y0 - s_y0) * v_dx) / cross;
-            double v_T = ((v_x0 - s_x0) * s_dy - (v_y0 - s_y0) * s_dx) / cross;
+            //checking if intersection point is on the segment & the ray
+            bool isOnS = 
+                x_i >= Math.Min(s_x0, s_x1) && 
+                x_i <= Math.Max(s_x0, s_x1) && 
+                y_i >= Math.Min(s_y0, s_y1) && 
+                y_i <= Math.Max(s_y0, s_y1);
+            bool isOnV = (v_dx * (x_i - v_x0) + v_dy * (y_i - v_y0)) > 0;
+            return isOnS && isOnV;
+        }
 
-            //check that intersection is on the ray (from the vector) and on the 
-            return s_T >= 0 && s_T <= 1 && v_T >= 0;
+        private static (double, double, double) GetLine(double x0, double y0, double x1, double y1)
+        {
+            double A = y1 - y0;
+            double B = x0 - x1;
+            double C = x1 * y0 - x0 * y1;
+            return (A, B, C);
         }
     }
 }
--- RobotPainter.Calculations/StrokeGeneration/StrokeGenerator.cs
+++ RobotPainter.Calculations/StrokeGeneration/StrokeGenerator.cs
@@ -29,13 +29,14 @@ namespace RobotPainter.Calculations.StrokeGeneration
         public double real_width_mm;
 
         public double real_stroke_max_length_mm;
-        public double StrokeMaxLength { get { return real_stroke_max_length_mm * width / real_width_mm; } }
+        //public double StrokeMaxLength { get { return real_stroke_max_length_mm * width / real_width_mm; } }
+        public double StrokeMaxLength = 80;
 
-        public double Ltol = 0.5;
+        public double Ltol = 5;
 
         private readonly IOptimizer _optimizer;
 
-        private List<Brushstroke> strokes = new List<Brushstroke>();
+        public List<Brushstroke> strokes = new List<Brushstroke>();
 
         public StrokeGenerator(LabBitmap target_image, int n_voronoi, IOptimizer optimizer, int n_rolling_avg = 7)
         {
@@ -53,6 +54,7 @@ namespace RobotPainter.Calculations.StrokeGeneration
         {
             ClearSitesList();
             siteToStroke.Clear();
+            strokes.Clear();
             var unprocessed_sites = sites.Select(x => x).ToList();
             while(unprocessed_sites.Count > 0)
             {
@@ -63,6 +65,7 @@ namespace RobotPainter.Calculations.StrokeGeneration
                     unprocessed_sites.Remove(site);
                     siteToStroke.Add(site, stroke);
                 }
+                strokes.Add(stroke);
             }
         }
 
@@ -135,35 +138,79 @@ namespace RobotPainter.Calculations.StrokeGeneration
         public LabBitmap GetColoredStrokeMap()
         {
             var result = new LabBitmap(width, height);
+            var mask = GetVoronoiMask();
             for(int x = 0; x < width; x++)
             {
                 for(int y = 0; y < height; y++)
                 {
-                    bool value_found = false;
-                    for (int i = 0; i < sites.Count; i++)
+                    result.SetPixel(x, y, siteToStroke[sites[mask[x, y]]].MainColor);
+                }
+            }
+            return result;
+        }
+
+        public int[,] GetVoronoiMask()
+        {
+            var result = new int[width, height];
+            //find first pixel
+            double min_d = double.MaxValue;
+            int last_site_i = -1;
+            for (int i = 0; i < sites.Count; i++)
+            {
+                double d = Math.Sqrt(Math.Pow(sites[i].X, 2) + Math.Pow(sites[i].Y, 2));
+                if (d < min_d)
+                {
+                    result[0, 0] = i;
+                    last_site_i = i;
+                    min_d = d;
+                }
+            }
+            //for every pixel after only check cells that are neighbors to the last found cell
+            for(int x = 0; x < width; x++)
+            {
+                //first go ->
+                for(int y = 0; y < height; y++)
+                {
+                    min_d = Math.Sqrt(Math.Pow(sites[last_site_i].X - x, 2) + Math.Pow(sites[last_site_i].Y - y, 2));
+                    var sites_to_check = sites[last_site_i].Neighbours;
+                    result[x, y] = last_site_i;
+                    foreach (var site in sites_to_check)
                     {
-                        if (sites[i].Contains(x, y))
+                        double d = Math.Sqrt(Math.Pow(site.X - x, 2) + Math.Pow(site.Y - y, 2));                         
+                        if (d < min_d)
                         {
-                            result.SetPixel(x, y, siteToStroke[sites[i]].MainColor);
-                            value_found = true;
-                            break;
+                            int i = sites.IndexOf(site);
+                            result[x, y] = i;
+                            last_site_i = i;
+                            min_d = d;
                         }
                     }
-                    if (!value_found)
+                }
+                //second go <-
+                x++;
+                if(x >= width)
+                {
+                    break;
+                }
+                for (int y = height - 1; y >= 0; y--)
+                {
+                    min_d = Math.Sqrt(Math.Pow(sites[last_site_i].X - x, 2) + Math.Pow(sites[last_site_i].Y - y, 2));
+                    var sites_to_check = sites[last_site_i].Neighbours;
+                    result[x, y] = last_site_i;
+                    foreach (var site in sites_to_check)
                     {
-                        double min_d = double.MaxValue;
-                        for (int i = 0; i < sites.Count; i++)
+                        double d = Math.Sqrt(Math.Pow(site.X - x, 2) + Math.Pow(site.Y - y, 2));
+                        if (d < min_d)
                         {
-                            double d = Math.Sqrt(Math.Pow(sites[i].X - x, 2) + Math.Pow(sites[i].Y - y, 2));
-                            if (d < min_d)
-                            {
-                                result.SetPixel(x, y, siteToStroke[sites[i]].MainColor);
-                                min_d = d;
-                            }
+                            int i = sites.IndexOf(site);
+                            result[x, y] = i;
+                            last_site_i = i;
+                            min_d = d;
                         }
                     }
                 }
             }
+
             return result;
         }
     }

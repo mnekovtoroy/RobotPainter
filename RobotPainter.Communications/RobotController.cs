c25378bd11856c13d825f158216746388077033f
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-05-04 00:03:55 +0300

    +remapping of the coordinates
    to match the robot coordinate system.
    Also improved photo retrieving stability

--- RobotPainter.Communications/RobotController.cs
+++ RobotPainter.Communications/RobotController.cs
@@ -1,5 +1,6 @@
 ﻿using PainterCore;
 using RobotPainter.Communications.Converting;
+using RobotPainter.Core;
 using System.Drawing;
 
 namespace RobotPainter.Communications
@@ -34,27 +35,34 @@ namespace RobotPainter.Communications
         private PaintingController _controller;
         private IPltConverter _pltConverter;
 
-        private RobotController(IPltConverter pltConverter, string temp_path, string photo_folder_path, PaintingController controller)
+        private double _canvasWidth;
+        private double _canvasHeight;
+
+        private RobotController(IPltConverter pltConverter, string temp_path, string photo_folder_path, double canvas_width, double canvas_height, PaintingController controller)
         {
             _tempPath = temp_path;
             _photoFolderPath = photo_folder_path;
             _pltConverter = pltConverter;
             _controller = controller;
+
+            _canvasWidth = canvas_width; ;
+            _canvasHeight = canvas_height;
         }
 
-        public static async Task<RobotController> Create(IPltConverter pltConverter, string temp_path, string photo_folder_path, string controllerIp = null)
+        public static async Task<RobotController> Create(IPltConverter pltConverter, string temp_path, string photo_folder_path, double canvas_width, double canvas_height, string controllerIp = null)
         {
             PaintingController controller;
             if (controllerIp == null)
                 controller = await PaintingController.CreateController();
             else
                 controller = await PaintingController.CreateController(controllerIp);
-            return new RobotController(pltConverter, temp_path, photo_folder_path, controller);
+            return new RobotController(pltConverter, temp_path, photo_folder_path, canvas_width, canvas_height, controller);
         }
 
         public async Task ApplyStrokes(List<BrushstrokeInfo> strokes)
         {
-            var pltCommandes = _pltConverter.ToPlt(strokes);
+            var strokes_mapped = RemapCoordinates(strokes);
+            var pltCommandes = _pltConverter.ToPlt(strokes_mapped);
             var path_plt = _tempPath + "commands.plt";
 
             _pltConverter.SavePlt(pltCommandes, path_plt);
@@ -66,28 +74,50 @@ namespace RobotPainter.Communications
         {
             await _controller.TakeaPhoto();
 
+            //waiting a sec for the photo to be sent to pc
+            Thread.Sleep(1000);
+
             //retrieve the photo
             string[] files = Directory.GetFiles(_photoFolderPath);
 
             var file_creation_times = new Dictionary<string, DateTime>();
 
-            foreach(string file in files)
+            Bitmap photo = null;
+            while(photo == null)
             {
-                file_creation_times.Add(file, File.GetCreationTime(file));
+                try
+                {
+                    foreach (string file in files)
+                    {
+                        file_creation_times.Add(file, File.GetCreationTime(file));
+                    }
+                    var files_sorted = file_creation_times.OrderBy(f => f.Value).ToList();
+
+                    photo = new Bitmap(files_sorted[0].Key);
+
+                    //deleting old files
+                    for (int i = 1; i < files_sorted.Count; i++)
+                    {
+                        File.Delete(files_sorted[i].Key);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to open or delete a file:");
+                    Console.WriteLine(ex.Message);
+                }
             }
-            var files_sorted = file_creation_times.OrderBy(f => f.Value).ToList();
-
-            Bitmap photo = new Bitmap(files_sorted[0].Key);
-
-            throw new NotImplementedException();
-            Thread.Sleep(1000);
-            //deleting old files
-            for(int i = 1; i < files_sorted.Count; i++)
-            {
-                File.Delete(files_sorted[i].Key);
-            }
-
             return photo;
+        }
+
+        private List<BrushstrokeInfo> RemapCoordinates(List<BrushstrokeInfo> strokes)
+        {
+            var remapped = strokes.Select(s => new BrushstrokeInfo()
+            {
+                Color = s.Color,
+                RootPath = s.RootPath.Select(p => new Point3D(p.x, _canvasHeight - p.y, p.z)).ToList()
+            }).ToList();
+            return remapped;
         }
 
         private async Task SendPltCommands(string path_plt)

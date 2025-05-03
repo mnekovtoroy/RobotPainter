214f20f320850cb493f2c8fb845bed71506af94b
boomerrank <75076193+mnekovtoroy@users.noreply.github.com>
2025-05-03 22:22:45 +0300

    Photo transforming

---
+++ RobotPainter.Application/PhotoTransforming/DummyTransformer.cs
@@ -0,0 +1,12 @@
+﻿namespace RobotPainter.Application.PhotoTransforming
+{
+    public class DummyTransformer : IPhotoTransformer
+    {
+        public Bitmap Transform(Bitmap bitmap, int target_width, int target_height)
+        {
+            //Thread.Sleep(10000);
+            var transformed = new Bitmap(bitmap, target_width, target_height);
+            return transformed;
+        }
+    }
+}
---
+++ RobotPainter.Application/PhotoTransforming/PhotoTransformer.cs
@@ -0,0 +1,39 @@
+﻿using RobotPainter.Calculations.ImageProcessing;
+
+namespace RobotPainter.Application.PhotoTransforming
+{
+    public class PhotoTransformer : IPhotoTransformer
+    {
+        private Point whitePoint = new Point(1803, 763);
+        private Point blackPoint = new Point(3015, 15);
+
+        private Point canvasUpperLeft = new Point(100, 100);
+        private Size canvasSize = new Size(1000, 2000);
+
+        private Rectangle canvasArea;
+
+        public PhotoTransformer()
+        {
+            canvasArea = new Rectangle(canvasUpperLeft, canvasSize);
+        }
+
+        public Bitmap Transform(Bitmap bitmap, int target_width, int target_height)
+        {
+            //cropping 
+            var cropped = bitmap.Clone(canvasArea, bitmap.PixelFormat);
+
+            //resizing
+            var resized = new Bitmap(cropped, new Size(target_width, target_height));
+
+            //white balance
+            Color perfect_white = bitmap.GetPixel(whitePoint.X, whitePoint.Y);
+            Color perfect_black = bitmap.GetPixel(blackPoint.X, blackPoint.Y);
+            ImageProcessor.WhiteBalance(resized, perfect_white, perfect_black);
+
+            //idk if its needed
+            cropped.Dispose();
+            resized.Dispose();
+            return resized;
+        }
+    }
+}
